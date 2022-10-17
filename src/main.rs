cecb10392cc6559b9b951293bb2d94a8d1600ad1
Ildar Shamiev <shamief@yandex.ru>
2022-10-17 14:58:11 +0300

    p.39 impl rest methods via pattern matching in handler

--- src/main.rs
+++ src/main.rs
@@ -45,9 +45,51 @@ fn microservice_handler(req: Request<Body>, user_db: &UserDb)
             (&Method::GET, "/") => {
                 Response::new(INDEX.into())
             },
+
             (method, path) if path.starts_with(USER_PATH) => {
-                unimplemented!();
+                let user_id = path.trim_start_matches(USER_PATH)
+                    .parse::<UserId>()
+                    .ok()
+                    .map(|x| x as usize);
+
+                let mut users = user_db.lock().unwrap();
+                match (method, user_id) {
+                    (&Method::POST, None) => {
+                        let id = users.insert(UserData);
+                        Response::new(id.to_string().into())
+                    },
+                    (&Method::POST, Some(_)) => {
+                        response_with_code(StatusCode::BAD_REQUEST)
+                    },
+                    (&Method::GET, Some(id)) => {
+                        if let Some(data) = users.get(id) {
+                            Response::new(data.to_string().into())
+                        } else {
+                            response_with_code(StatusCode::NOT_FOUND)
+                        }
+                    },
+                    (&Method::PUT, Some(id)) => {
+                        if let Some(user) = users.get_mut(id) {
+                            *user = UserData;
+                            response_with_code(StatusCode::OK)
+                        } else {
+                            response_with_code(StatusCode::NOT_FOUND)
+                        }
+                    },
+                    (&Method::DELETE, Some(id)) => {
+                        if users.contains(id) {
+                            users.remove(id);
+                            response_with_code(StatusCode::OK)
+                        } else {
+                            response_with_code(StatusCode::NOT_FOUND)
+                        }
+                    },
+                    _ => {
+                        response_with_code(StatusCode::METHOD_NOT_ALLOWED)
+                    },
+                }
             },
+
             _ => {
                 response_with_code(StatusCode::NOT_FOUND)
             },
@@ -64,3 +106,9 @@ fn response_with_code(status_code: StatusCode) -> Response<Body> {
         .unwrap()
 }
 
+
+impl fmt::Display for UserData {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str("{}")
+    }
+} 
