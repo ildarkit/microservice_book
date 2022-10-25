1b74742f588eff794a05d1fcb239ff23662c0277
Ildar Shamiev <shamief@yandex.ru>
2022-10-25 18:48:16 +0300

    p.135 impl client and server bin crates

--- chapter-6/grpc-ring/src/client.rs
+++ chapter-6/grpc-ring/src/client.rs
@@ -1,3 +1,11 @@
-fn main() {
-    todo!();
+use failure::Error;
+use grpc_ring::Remote;
+use std::env;
+
+
+fn main() -> Result<(), Error> {
+    let next = env:var("NEXT")?.parse()?;
+    let remote = Remote::new(next)?;
+    remote.start_roll_call()?;
+    Ok(())
 }
