b548de1ce3fcd84b160a9bdea9aaac1741f402da
Ildar Shamiev <shamief@yandex.ru>
2022-11-05 12:05:49 +0300

    pass connection as arg into api

--- chapter-8/chat/src/lib.rs
+++ chapter-8/chat/src/lib.rs
@@ -10,6 +10,7 @@ use self::schema::{channels, memberships, messages, users};
 use diesel::{r2d2::{Pool, ConnectionManager, PooledConnection}, insert_into};
 
 pub type PoolConnection = Pool<ConnectionManager<PgConnection>>;
+pub type PooledConn = PooledConnection<ConnectionManager<PgConnection>>;
 
 pub struct Api
 {
@@ -25,23 +26,26 @@ impl Api {
         Ok(Self { pool })
     }
 
-    fn get_connect(&self)
-        -> Result<PooledConnection<ConnectionManager<PgConnection>>, Error>
+    pub fn get_connect(&self)
+        -> Result<PooledConn, Error>
     {
         self.pool.get().map_err(Error::from)
     }
 
-    pub fn register_user(&mut self, email: &str) -> Result<User, Error> {
+    pub fn register_user(&self, conn: &mut PooledConn, email: &str)
+        -> Result<User, Error>
+    {
         insert_into(users::table)
             .values((users::email.eq(email),))
-            .get_result(&mut self.get_connect().unwrap())
+            .get_result(conn)
             .map_err(Error::from)
     }
 
-    pub fn create_channel(&mut self, user_id: Id, title: &str, is_public: bool)
+    pub fn create_channel(
+        &self, conn: &mut PooledConn, user_id: Id, title: &str, is_public: bool
+        )
         -> Result<Channel, Error>
     {
-        let conn = &mut self.get_connect().unwrap();
         conn.transaction(|conn| {
             let channel: Channel = insert_into(channels::table)
                 .values((
@@ -51,19 +55,14 @@ impl Api {
                         ))
                 .get_result(conn)
                 .map_err(Error::from)?;
-            insert_into(memberships::table)
-            .values((
-                    memberships::channel_id.eq(channel.id),
-                    memberships::user_id.eq(user_id),
-                    ))
-            .execute(conn)
-            .map_err(Error::from)?;
+            self.add_member(conn, channel.id, user_id)?;
             Ok(channel)
         })
     }
 
-    pub fn publish_channel(&mut self, channel_id: Id) -> Result<(), Error> {
-        let conn = &mut self.get_connect().unwrap();
+    pub fn publish_channel(&self, conn: &mut PooledConn, channel_id: Id)
+        -> Result<(), Error>
+    {
         let channel = channels::table
             .filter(channels::id.eq(channel_id))
             .select((
@@ -87,7 +86,7 @@ impl Api {
         }
     }
 
-    pub fn add_member(&mut self, channel_id: Id, user_id: Id) 
+    pub fn add_member(&self, conn: &mut PooledConn, channel_id: Id, user_id: Id) 
         -> Result<Membership, Error>
     {
         insert_into(memberships::table)
@@ -95,11 +94,11 @@ impl Api {
                     memberships::channel_id.eq(channel_id),
                     memberships::user_id.eq(user_id),
                     ))
-            .get_result(&mut self.get_connect().unwrap())
+            .get_result(conn)
             .map_err(Error::from)
     }
 
-    pub fn add_message(&mut self, channel_id: Id, user_id: Id, text: &str)
+    pub fn add_message(&self, conn: &mut PooledConn, channel_id: Id, user_id: Id, text: &str)
         -> Result<Message, Error> 
     {
         let ts_now = Utc::now().naive_utc();
@@ -110,14 +109,16 @@ impl Api {
                     messages::user_id.eq(user_id),
                     messages::text.eq(text)
                     ))
-            .get_result(&mut self.get_connect().unwrap())
+            .get_result(conn)
             .map_err(Error::from)
     }
 
-    pub fn delete_message(&mut self, message_id: Id) -> Result<(), Error> {
+    pub fn delete_message(&self, conn: &mut PooledConn, message_id: Id)
+        -> Result<(), Error>
+    {
         diesel::delete(messages::table)
             .filter(messages::id.eq(message_id))
-            .execute(&mut self.get_connect().unwrap())?;
+            .execute(conn)?;
         Ok(())
     }
 }
@@ -128,14 +129,15 @@ mod tests {
 
     #[test]
     fn create_users() {
-        let mut api = Api::connect().unwrap();
-        let user_1 = api.register_user("user_1@example.com").unwrap();
-        let user_2 = api.register_user("user_2@example.com").unwrap();
-        let channel = api.create_channel(user_1.id, "My Channel", false).unwrap();
-        api.publish_channel(channel.id).unwrap();
-        api.add_member(channel.id, user_2.id).unwrap();
-        let message = api.add_message(channel.id, user_1.id, "Welcome!").unwrap();
-        api.add_message(channel.id, user_2.id, "Hi!").unwrap();
-        api.delete_message(message.id).unwrap();
+        let api = Api::connect().unwrap();
+        let conn = &mut api.get_connect().unwrap();
+        let user_1 = api.register_user(conn, "user_1@example.com").unwrap();
+        let user_2 = api.register_user(conn, "user_2@example.com").unwrap();
+        let channel = api.create_channel(conn, user_1.id, "My Channel", false).unwrap();
+        api.publish_channel(conn, channel.id).unwrap();
+        api.add_member(conn, channel.id, user_2.id).unwrap();
+        let message = api.add_message(conn, channel.id, user_1.id, "Welcome!").unwrap();
+        api.add_message(conn, channel.id, user_2.id, "Hi!").unwrap();
+        api.delete_message(conn, message.id).unwrap();
     }
 }
