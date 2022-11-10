a5bfff3728e747120d5f6b671625d2bca0b0d274
Ildar Shamiev <shamief@yandex.ru>
2022-11-10 19:23:19 +0300

    tracing

--- chapter-9/gotham-postgres-store/src/main.rs
+++ chapter-9/gotham-postgres-store/src/main.rs
@@ -1,18 +1,20 @@
 use std::sync::Arc;
 use failure::Error;
-use gotham::handler::HandlerResult;//{HandlerError, HandlerResult};
+use gotham::handler::HandlerResult;
 use gotham::router::Router;
-use gotham::router::build_simple_router;//{DefineSingleRoute, DrawRoutes, build_router};
+use gotham::router::build_simple_router;
 use gotham::state::{FromState, State};
 use gotham::mime::TEXT_HTML_UTF_8;
 use gotham::helpers::http::response::create_response;
 use gotham::prelude::*;
-use hyper::StatusCode;//{Body, Response, StatusCode};
+use hyper::StatusCode;
 use hyper::header::{HeaderMap, USER_AGENT};
 use tokio::runtime::Runtime;
 use tokio_postgres::NoTls;
 use tokio::sync::mpsc::{self, Sender, Receiver};
 use tokio::sync::Mutex;
+use tracing::{instrument, trace, info, error};
+use tracing_subscriber;
 #[macro_use]
 extern crate lazy_static;
 
@@ -46,6 +48,7 @@ lazy_static! {
     };
 }
 
+#[instrument]
 fn router() -> Router {
     build_simple_router(|route| {
         route
@@ -54,6 +57,7 @@ fn router() -> Router {
     })
 } 
 
+#[instrument(skip(state))]
 async fn register_user_agent(state: State) -> HandlerResult {
     let user_agent = HeaderMap::borrow_from(&state)
         .get(USER_AGENT)
@@ -62,48 +66,63 @@ async fn register_user_agent(state: State) -> HandlerResult {
 
     let mutex_sender = STATUS_CHANNEL.lock().await;
     let sender = mutex_sender.get_sender();
+    trace!("Sending to channel");
     let (status, body) = match sender.send(user_agent.to_string()).await {
         Ok(_) => {
+            trace!("Sended successfully");
             (StatusCode::OK,
             format!("User-Agent: {}",user_agent))
         }
         Err(err) => {
+            error!("Channel sending fail");
             (StatusCode::INTERNAL_SERVER_ERROR,
             err.to_string())
         }
     };
-
+    trace!("Responding");
     let res = create_response(
         &state,
         status,
         TEXT_HTML_UTF_8,
         body);
+    trace!("Return response");
     Ok((state, res))
 }
 
 fn main() -> Result<(), Error> {
+    let subscriber = tracing_subscriber::fmt()
+        .compact()
+        .with_file(true)
+        .with_line_number(true)
+        .with_thread_ids(true)
+        .with_target(false)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)?;
+
     let rt = Runtime::new().unwrap();
     let handshake =
         tokio_postgres::connect("postgres://postgres@localhost:5432", NoTls);
     let (client, connection) = rt.block_on(handshake)?;
     rt.spawn(async move {
         match connection.await {
-            Ok(res) => Ok(res),
+            Ok(res) => {
+                trace!("Connected to database");
+                Ok(res)
+            }
             Err(e) => {
-                eprintln!("connection error: {}", e);
+                error!("Connection error: {}", e);
                 Err(e)
             }
         }
     });
-
     rt.block_on(async {
         client.batch_execute(
         "CREATE TABLE IF NOT EXISTS agents (
             agent TEXT NOT NULL,
             timestamp TIMESTAMPTZ NOT NULL DEFAULT NOW()
             );").await.unwrap();
+        trace!("Table created");
     });
-
     rt.spawn(async move {
         let mut rx_mutex = STATUS_CHANNEL.lock().await;
         let rx = rx_mutex.get_receiver();
@@ -111,11 +130,11 @@ fn main() -> Result<(), Error> {
             client.query("INSERT INTO agents (agent) VALUES ($1) RETURNING agent", 
                          &[&user_agent])
                 .await.unwrap();
+            trace!("Data was saved in database");
         }
     });
-
     let addr = "127.0.0.1:7878";
-    println!("Listening for requests at http://{}", addr);
+    info!("Listening for requests at http://{}", addr);
     gotham::start(addr, router()).unwrap();
 
     Ok(())
