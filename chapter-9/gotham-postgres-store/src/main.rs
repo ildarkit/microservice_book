6f5d98be9c455dd9eceea68ba0d470611da9be1f
Ildar Shamiev <shamief@yandex.ru>
2022-11-10 17:50:09 +0300

    resolve compiler errors

--- chapter-9/gotham-postgres-store/src/main.rs
+++ chapter-9/gotham-postgres-store/src/main.rs
@@ -1,10 +1,6 @@
-use std::ops::{Deref, DerefMut};
 use std::sync::Arc;
 use failure::Error;
 use gotham::handler::HandlerResult;//{HandlerError, HandlerResult};
-//use gotham::middleware::state::StateMiddleware;
-//use gotham::pipeline::single_pipeline;
-//use gotham::pipeline::single_middleware;
 use gotham::router::Router;
 use gotham::router::build_simple_router;//{DefineSingleRoute, DrawRoutes, build_router};
 use gotham::state::{FromState, State};
@@ -13,7 +9,7 @@ use gotham::helpers::http::response::create_response;
 use gotham::prelude::*;
 use hyper::StatusCode;//{Body, Response, StatusCode};
 use hyper::header::{HeaderMap, USER_AGENT};
-use tokio::task;
+use tokio::runtime::Runtime;
 use tokio_postgres::NoTls;
 use tokio::sync::mpsc::{self, Sender, Receiver};
 use tokio::sync::Mutex;
@@ -21,41 +17,25 @@ use tokio::sync::Mutex;
 extern crate lazy_static;
 
 struct StatusChannel {
-    tx: Option<Sender<String>>,
-    rx: Option<Receiver<String>>,
-}
-
-impl Deref for StatusChannel {
-    type Target = Option<Receiver<String>>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.rx
-    }
-}
-
-impl DerefMut for StatusChannel {
-    //type Target = Option<Receiver<String>>;
-
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.rx
-    }
+    tx: Sender<String>,
+    rx: Receiver<String>,
 }
 
 impl StatusChannel {
     fn new() -> Self {
         let (tx, rx) = mpsc::channel(100);
         Self {
-            tx: Some(tx),
-            rx: Some(rx)
+            tx,
+            rx
         }
     }
 
-    fn get_sender(&self) -> Option<&Sender<String>> {
-        self.tx.as_ref()
+    fn get_sender(&self) -> &Sender<String> {
+        &self.tx
     }
 
-    fn get_receiver(&mut self) -> Option<&mut Receiver<String>> {
-        self.rx.as_mut()
+    fn get_receiver(&mut self) -> &mut Receiver<String> {
+        &mut self.rx
     }
 }
 
@@ -81,7 +61,7 @@ async fn register_user_agent(state: State) -> HandlerResult {
         .unwrap_or_else(|| "<undefined>");
 
     let mutex_sender = STATUS_CHANNEL.lock().await;
-    let sender = mutex_sender.get_sender().unwrap();
+    let sender = mutex_sender.get_sender();
     let (status, body) = match sender.send(user_agent.to_string()).await {
         Ok(_) => {
             (StatusCode::OK,
@@ -101,27 +81,32 @@ async fn register_user_agent(state: State) -> HandlerResult {
     Ok((state, res))
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Error> {
-    let (client, connection) =
-        tokio_postgres::connect("postgres://postgres@localhost:5432", NoTls).await?;
-    tokio::spawn(async move {
-        if let Err(e) = connection.await {
-            eprintln!("connection error: {}", e);
+fn main() -> Result<(), Error> {
+    let rt = Runtime::new().unwrap();
+    let handshake =
+        tokio_postgres::connect("postgres://postgres@localhost:5432", NoTls);
+    let (client, connection) = rt.block_on(handshake)?;
+    rt.spawn(async move {
+        match connection.await {
+            Ok(res) => Ok(res),
+            Err(e) => {
+                eprintln!("connection error: {}", e);
+                Err(e)
+            }
         }
     });
 
-    let execute = client.batch_execute(
+    rt.block_on(async {
+        client.batch_execute(
         "CREATE TABLE IF NOT EXISTS agents (
             agent TEXT NOT NULL,
             timestamp TIMESTAMPTZ NOT NULL DEFAULT NOW()
-            );");
-    task::spawn_blocking(move || execute).await?;
+            );").await.unwrap();
+    });
 
-    let mut rx_mutex = STATUS_CHANNEL.lock().await;
-    let rx = &mut rx_mutex.get_receiver().unwrap();
-    
-    tokio::spawn(async {
+    rt.spawn(async move {
+        let mut rx_mutex = STATUS_CHANNEL.lock().await;
+        let rx = rx_mutex.get_receiver();
         while let Some(user_agent) = rx.recv().await {
             client.query("INSERT INTO agents (agent) VALUES ($1) RETURNING agent", 
                          &[&user_agent])
