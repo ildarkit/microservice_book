cefb6851d2aa2f75971eb21cfafc3849fa35b6fc
Ildar Shamiev <shamief@yandex.ru>
2022-12-31 10:56:15 +0300

    server mod

--- chapter-12/actix-rabbitmq-qr/src/server.rs
+++ chapter-12/actix-rabbitmq-qr/src/server.rs
@@ -1,52 +1,14 @@
-use actix::{Addr, System};
-use actix_web::http::{self, header, StatusCode};
-use actix_web::{rt, middleware, server, App, Error as WebError,
-    HttpMessage, HttpRequest, HttpResponse, HttpServer};
-use askama::Template;
-use chrono::{DateTime, Utc};
-use futures::{future, Future, Stream};
-use indexmap::IndexMap;
-use log::debug;
-use actix_rabbitmq_qr::queue_actor::{QueueActor, QueueHandler, SendMessage, TaskId};
+mod handlers;
+
+use anyhow::Error;
+use actix_web::{web, middleware::Logger, App, HttpServer};
+use actix_rabbitmq_qr::queue_actor::{QueueActor, QueueHandler, TaskId};
 use actix_rabbitmq_qr::{QrRequest, QrResponse, REQUESTS, RESPONSES};
-use actix_rabbitmq_qr::handlers;
-use std::fmt;
-use std::sync::{Arc, Mutex};
-
-type SharedTasks = Arc<Mutex<IndexMap<String, Record>>>;
+use actix_rabbitmq_qr::state::State;
+use actix_rabbitmq_qr::state::tasks::{SharedTasks, init_tasks, Status};
 
 #[derive(Clone)]
-struct Record {
-    task_id: TaskId,
-    timestamp: DateTime<Utc>,
-status: Status,
-}
-
-#[derive(Clone)]
-enum Status {
-    InProgress,
-    Done(QrResponse),
-}
-
-impl fmt::Display for Status {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Status::InProgress => write!(f, "in progress"),
-            Status::Done(resp) => match resp {
-                QrResponse::Succeed(data) => write!(f, "done: {data}"),
-                QrResponse::Failed(err) => write!(f, "failed: {err}"),
-            },
-        }
-    }
-}
-
-#[derive(Clone)]
-struct State {
-    tasks: SharedTasks,
-    addr: Addr<QueueActor<ServerHandler>>,
-}
-
-struct ServerHandler {
+pub struct ServerHandler {
     tasks: SharedTasks,
 }
 
@@ -67,40 +29,40 @@ impl QueueHandler for ServerHandler {
     {
         self.tasks.lock().unwrap().get_mut(id)
             .map(move |rec| {
-                rec.status = Status::Done(incoming);
+                rec.set_status(Status::Done(incoming));
             });
         Ok(None)
     }
 }
 
-fn main() -> std::io::Result<()> {
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
-    let mut sys = rt::System::new();
-    let tasks = Arc::new(Mutex::new(IndexMap::new()));
+    let addr = "amqp://127.0.0.1:5672";
+    let tasks = init_tasks(); 
     let addr = QueueActor::new(
         ServerHandler {
             tasks: tasks.clone(),
         },
-        &mut sys,
-    )?;
-
-    let state = State {
-        tasks: tasks.clone(),
         addr,
-    };
-
-    sys.block_on(
-        HttpServer::new(move || { 
-            let data = web::Data::new(state);
-            App::new()
-                .wrap(Logger::default())
-                .app_data(web::Data::clone(&data))
-                .route("/", web::get().to(handlers::index))
-                .route("/task", web::post().to(handlers::upload))
-                .route("/tasks", web::get().to(handlers::tasks)) 
-        })
-        .bind(("127.0.0.1", 8080))?
-        .run()
-    )
+    ).await.unwrap(); 
+ 
+    HttpServer::new(move || {
+        let state = State::new(
+            tasks.clone(),
+            addr.clone(),
+        );
+        let data = web::Data::new(state);
+        App::new()
+            .wrap(Logger::default())
+            .app_data(web::Data::clone(&data))
+            .route("/", web::get().to(handlers::index))
+            .route("/task", web::post().to(handlers::upload))
+            .route("/tasks", web::get().to(handlers::tasks)) 
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await?;
+    Ok(())
 }
