3f6155288161ee06d26ae3a5e6188946eff7b1fc
Ildar Shamiev <shamief@yandex.ru>
2023-01-04 13:00:06 +0300

    server run with start fn

--- chapter-13/actix_web-route-request-tests/src/main.rs
+++ chapter-13/actix_web-route-request-tests/src/main.rs
@@ -1,54 +1,73 @@
 use actix_files as fs;
-use actix::prelude::SyncArbiter;
+use actix::prelude::{System, SyncArbiter};
 use actix_identity::IdentityMiddleware;
 use actix_session::{storage::CookieSessionStore, SessionMiddleware};
 use actix_web::{cookie::Key, middleware::Logger, web, App, HttpServer};
 
+use request_count::LinksMap;
 use request_count::counter::CountState;
 use request_count::middleware::Counter;
 use request_count::handlers;
 use request_count::cache::{CacheLink, CacheActor};
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
+fn read_config() -> std::io::Result<LinksMap> {
+    let links = std::fs::read_to_string("links.toml")?;
+    Ok(toml::from_str(&links)?)
+}
+
+fn start(links: LinksMap) -> std::io::Result<()> {
+    let sys = System::new();
+
+    sys.block_on(
+        async move {
+            let secret = Key::generate();
+            let addr = SyncArbiter::start(3, || {
+                CacheActor::new("redis://127.0.0.1:6379", 10)
+            });
+            let cache = CacheLink::new(addr);
+
+            HttpServer::new(move || {
+                let state = CountState::new(cache.clone(), links.clone());
+                let data = web::Data::new(state);
+                App::new()
+                    .wrap(IdentityMiddleware::default())
+                    .wrap(
+                        SessionMiddleware::builder(
+                            CookieSessionStore::default(),
+                            secret.clone()
+                        )
+                        .cookie_name("auth-example".to_owned())
+                        .cookie_secure(false)
+                        .build()
+                    )
+                    .wrap(Logger::default())
+                    .wrap(Counter)
+                    .app_data(web::Data::clone(&data))
+                    .service(
+                        web::scope("/api")
+                            .route("/singup", web::post().to(handlers::signup))
+                            .route("/signin", web::post().to(handlers::signin))
+                            .route("/new_comment", web::post().to(handlers::new_comment))
+                            .route("/comments", web::get().to(handlers::comments))
+                    )
+                    .route("stats/counter", web::get().to(handlers::counter))
+                    .service(
+                        fs::Files::new("/", "./static").index_file("index.html")
+                    )
+            })
+            .workers(1)
+            .bind("127.0.0.1:8080")?
+            .run()
+            .await
+        }
+    )?;
+    sys.run()
+}
+
+fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
-    let secret = Key::generate();
-
-    let addr = SyncArbiter::start(3, || {
-        CacheActor::new("redis://127.0.0.1:6379", 10)
-    });
-    let cache = CacheLink::new(addr); 
-
-    HttpServer::new(move || {
-        let state = CountState::new(cache.clone());
-        let data = web::Data::new(state);
-        App::new()
-            .wrap(IdentityMiddleware::default())
-            .wrap(
-                SessionMiddleware::builder(CookieSessionStore::default(), secret.clone())
-                    .cookie_name("auth-example".to_owned())
-                    .cookie_secure(false)
-                    .build()
-            )
-            .wrap(Logger::default())
-            .wrap(Counter)
-            .app_data(web::Data::clone(&data))
-            .service(
-                web::scope("/api")
-                    .route("/singup", web::post().to(handlers::signup))
-                    .route("/signin", web::post().to(handlers::signin))
-                    .route("/new_comment", web::post().to(handlers::new_comment))
-                    .route("/comments", web::get().to(handlers::comments))
-            )
-            .route("stats/counter", web::get().to(handlers::counter))
-            .service(
-                fs::Files::new("/", "./static").index_file("index.html")
-            )
-    })
-    .workers(1)
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    let links = read_config()?;
+    start(links)
 }
 
 #[cfg(test)]
@@ -58,9 +77,10 @@ mod tests {
     use std::time::Duration;
     use lazy_static::lazy_static;
     use mockito::{mock, Mock};
-    use request::Client;
+    use reqwest::blocking::Client;
     use serde::{Deserialize, Serialize};
     use super::*;
+    use super::handlers::*;
 
     #[derive(Deserialize, Serialize)]
     struct Comment {
@@ -105,7 +125,7 @@ mod tests {
                     new_comment: mock_url(&url, "/new_comment"),
                     comments: mock_url(&url, "/comments"),
                 };
-                start(links);
+                start(links).unwrap();
             });
             thread::sleep(Duration::from_secs(5));
             *started = true;
@@ -114,47 +134,6 @@ mod tests {
 
     fn mock_url(base: &str, path: &str) -> String {
         format!("{}{}", base, path)
-    }
-
-    fn start(links: LinksMap) {
-        let sys = actix::System::new("router");
-        let state = State {
-            counter: Refcell::default(),
-            links,
-        };
-        sys.block_on(
-            HttpServer::new(move || {
-                let data = web::Data::new(state);
-                App::new()
-                    .wrap(IdentityMiddleware::default())
-                    .wrap(
-                        SessionMiddleware::builder(
-                            CookieSessionStore::default(),
-                            secret.clone()
-                        )
-                        .cookie_name("auth-example".to_owned())
-                        .cookie_secure(false)
-                        .build()
-                    )
-                    .wrap(Logger::default())
-                    .wrap(Counter)
-                    .app_data(web::Data::clone(&data))
-                    .service(
-                        web::scope("/api")
-                            .route("/singup", web::post().to(handlers::signup))
-                            .route("/signin", web::post().to(handlers::signin))
-                            .route("/new_comment", web::post().to(handlers::new_comment))
-                            .route("/comments", web::get().to(handlers::comments))
-                    )
-                    .route("stats/counter", web::get().to(handlers::counter))
-                    .service(
-                        fs::Files::new("/", "./static").index_file("index.html")
-                    )
-            })
-            .workers(1)
-            .bind("127.0.0.1:8080").unwrap().run()
-        ).unwrap();
-        sys.run().unwrap();
     }
 
     fn test_get<T>(path: &str) -> T
@@ -208,6 +187,6 @@ mod tests {
 
     #[test]
     fn test_list_with_client() {
-        let _ = Vec<Comment> = test_get("/comments");
+        let _: Vec<Comment> = test_get("/comments");
     }
 }
