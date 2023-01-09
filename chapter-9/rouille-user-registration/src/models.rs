3a2529fba7540b45f9e1e6f865ddff0c2f5381f6
Ildar Shamiev <shamief@yandex.ru>
2023-01-09 12:27:13 +0300

    p.220 rouille user registration microservice

---
+++ chapter-9/rouille-user-registration/src/models.rs
@@ -0,0 +1,18 @@
+use serde_derive::Serialize;
+use diesel::prelude::*;
+use super::schema::users;
+
+#[derive(Debug, Serialize, Queryable)]
+pub struct User {
+    pub id: String,
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Insertable)]
+#[diesel(table_name = users)]
+pub struct NewUser<'a> {
+    pub id: &'a str,
+    pub email: &'a str,
+    pub password: &'a str,
+}
