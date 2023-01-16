f46b6f6bcb40a03d1d13c8899429211848dce97c
Ildar Shamiev <shamief@yandex.ru>
2023-01-16 11:51:37 +0300

    fix cache response

--- chapter-15/microservices/router/src/cache.rs
+++ chapter-15/microservices/router/src/cache.rs
@@ -10,28 +10,42 @@ pub async fn cache(
 )
     -> Result<Vec<u8>, ClientHttpError>
 {
-    let res = cache.get_value(&cache_key).await;
-    let res =
-        if res.is_err() {
-            res.map_err(|e| {
-                let ctx = "Failed to get from the cache";
-                log::warn!("{ctx}\n Caused by:\n\t{e}"); 
-            }).ok();
-            let data = client::get_request(&client_addr).await
-                .map_err(|e| {
-                    let ctx = "Failed to get response from service"; 
-                    log::error!("{ctx}\n Caused by:\n\t{e}");
-                    e
-                })?;
-            cache.set_value(&cache_key, &data).await
-                .map_err(|e| {
-                    let ctx = "Failed to set to the cache";
-                    log::warn!("{ctx}\n Caused by:\n\t{e}");
-                }).ok();
-            data.to_vec()
-        } else {
+    let res = cache.get_value(&cache_key)
+        .await
+        .map_err(|e| {
+            let ctx = "Failed to get from the cache";
+            log::warn!("{ctx}\n Caused by:\n\t{e}"); 
+        })
+        .ok();
+    let res = match res {
+        Some(Some(res)) => {
             log::debug!("Received cached response");
-            res.unwrap().unwrap()
-        };
+            res
+        },
+        Some(None) | None => {
+            get_data(cache, cache_key, client_addr).await?
+        },
+    };
     Ok(res)
+}
+
+async fn get_data(
+    cache: &CacheLink,
+    cache_key: &str,
+    client_addr: &str
+)
+    -> Result<Vec<u8>, ClientHttpError>
+{
+    let data = client::get_request(client_addr).await
+        .map_err(|e| {
+            let ctx = "Failed to get response from service"; 
+            log::error!("{ctx}\n Caused by:\n\t{e}");
+            e
+        })?;
+    cache.set_value(&cache_key, &data).await
+        .map_err(|e| {
+            let ctx = "Failed to set to the cache";
+            log::warn!("{ctx}\n Caused by:\n\t{e}");
+        }).ok();
+    Ok(data)
 }
