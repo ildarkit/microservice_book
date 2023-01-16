cdc200ee44f4a5c32c371e0162c90289c52fabe9
Ildar Shamiev <shamief@yandex.ru>
2023-01-16 14:12:34 +0300

    fix new_comment response

--- chapter-15/microservices/content/src/main.rs
+++ chapter-15/microservices/content/src/main.rs
@@ -15,20 +15,28 @@ use comment::{Comment, NewComment};
 #[database("postgres_database")]
 pub struct Db(PgConnection);
 
+#[get("/")]
+async fn index() -> &'static str {
+    "Content Microservice"
+}
+
 #[get("/comments")]
 async fn list(conn: Db) -> Json<Vec<Comment>> {
     conn.run(|c| Json(Comment::all(&c))).await
 }
 
 #[post("/new_comment", data = "<comment_form>")]
-async fn add_new(comment_form: Form<NewComment>, conn: Db) {
+async fn add_new(comment_form: Form<NewComment>, conn: Db)
+    -> Json<()>
+{
     let comment = comment_form.into_inner();
     conn.run(|c| Comment::insert(comment, &c)).await;
+    Json(())
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .attach(Db::fairing())
-        .mount("/", routes![list, add_new])
+        .mount("/", routes![index, list, add_new])
 }
