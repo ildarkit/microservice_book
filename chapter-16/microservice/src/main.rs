6a0cc1844a307ef4ffc63b9f831d79327083fc27
Ildar Shamiev <shamief@yandex.ru>
2023-01-21 11:05:00 +0300

    cargo fmt

--- chapter-16/microservice/src/main.rs
+++ chapter-16/microservice/src/main.rs
@@ -9,11 +9,8 @@ async fn greet(name: web::Path<String>) -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
-    HttpServer::new(|| {
-        App::new().service(greet)
-            .wrap(Logger::default())
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(greet).wrap(Logger::default()))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
